e5fc24150b2ef01269c93a27bcf1a395850adf88
Gurpreet Singh <g.singh6343@gmail.com>
2016-10-03 17:48:23 +0530

    Update API results

--- generateAds.php
+++ generateAds.php
@@ -1,18 +1,14 @@
 <?php
 
-$tags = explode("$", $_GET['tags']);
+$tag = explode("$", $_GET['tags'])[1];
 $data = "{\"data\" : [";
+$content = simplexml_load_string(file_get_contents("http://svcs.sandbox.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=Gurpreet-Picandy-SBX-a2f5817e2-9d000f93&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&keywords=$tag"));
 
-foreach ($tags as $tag) {
-    if ($tag != "") {
-        $content = simplexml_load_string(file_get_contents("http://svcs.sandbox.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=Gurpreet-Picandy-SBX-a2f5817e2-9d000f93&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&keywords=$tag"));
-        $image = $content->searchResult->item[0]->galleryURL;
-        $url = $content->searchResult->item[0]->viewItemURL;
-        $title = $content->searchResult->item[0]->title;
+for ($i = 0; $i < 4; $i += 1) {
+    $url = $content->searchResult->item[$i]->viewItemURL;
+    $title = $content->searchResult->item[$i]->title;
 
-//        echo "<div class='image-ad'><a href='$url'>$title</a></div>";
-        $data = $data . "{\"url\" : \"$url\", \"title\" : \"$title\"}, ";
-    }
+    $data = $data . "{\"url\" : \"$url\", \"title\" : \"$title\"}, ";
 }
 
 $data = substr($data, 0, -2) . "]}";
